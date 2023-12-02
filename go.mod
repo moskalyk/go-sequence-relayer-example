f03bd15a9120a906396dc4b0de174eb98062d96f
Peter Kieltyka <peter.kieltyka@gmail.com>
2023-12-01 20:15:00 -0500

    update relayer example

--- go.mod
+++ go.mod
@@ -4,51 +4,43 @@ go 1.20
 
 require (
 	github.com/0xsequence/ethkit v1.22.4
-	github.com/0xsequence/go-sequence v0.24.3
+	github.com/0xsequence/go-sequence v0.25.0
 	github.com/ethereum/go-ethereum v1.13.5
 	github.com/goware/logger v0.3.0
 )
 
 require (
 	github.com/0xsequence/go-ethauth v0.13.0 // indirect
-	github.com/bits-and-blooms/bitset v1.7.0 // indirect
 	github.com/btcsuite/btcd v0.23.4 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
-	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.3 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/consensys/bavard v0.1.13 // indirect
-	github.com/consensys/gnark-crypto v0.12.1 // indirect
-	github.com/crate-crypto/go-kzg-4844 v0.7.0 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/ethereum/c-kzg-4844 v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/goware/breaker v0.1.2 // indirect
-	github.com/goware/cachestore v0.8.0 // indirect
+	github.com/goware/cachestore v0.8.1 // indirect
 	github.com/goware/calc v0.2.0 // indirect
 	github.com/goware/channel v0.2.4 // indirect
 	github.com/goware/singleflight v0.2.0 // indirect
 	github.com/goware/superr v0.0.2 // indirect
-	github.com/hashicorp/golang-lru/v2 v2.0.5 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mmcloughlin/addchain v0.4.0 // indirect
-	github.com/redis/go-redis/v9 v9.0.5 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/redis/go-redis/v9 v9.3.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
-	rsc.io/tmplfunc v0.0.3 // indirect
 )
--- main.go
+++ main.go
@@ -7,85 +7,31 @@ import (
 	"math/big"
 	"net/http"
 	"strings"
-	"time"
 
-	"github.com/0xsequence/ethkit/ethmonitor"
-	"github.com/0xsequence/ethkit/ethreceipts"
 	"github.com/0xsequence/ethkit/ethrpc"
-	"github.com/0xsequence/ethkit/ethtxn"
 	"github.com/0xsequence/ethkit/ethwallet"
 	"github.com/0xsequence/ethkit/go-ethereum/common"
 	"github.com/0xsequence/go-sequence"
-	"github.com/0xsequence/go-sequence/core"
 	v2 "github.com/0xsequence/go-sequence/core/v2"
 	"github.com/0xsequence/go-sequence/relayer"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/goware/logger"
 )
 
-type MetaTxnID string
-
-type MetaTxnStatus uint8
-
-type Transaction struct {
-	DelegateCall  bool           `abi:"delegateCall"`  // Performs delegatecall
-	RevertOnError bool           `abi:"revertOnError"` // Reverts transaction bundle if tx fails
-	GasLimit      *big.Int       `abi:"gasLimit"`      // Maximum gas to be forwarded
-	To            common.Address `abi:"target"`        // Address to send transaction, aka target
-	Value         *big.Int       `abi:"value"`         // Amount of ETH to pass with the call
-	Data          []byte         `abi:"data"`          // Calldata to pass
-
-	Transactions Transactions // Child transactions
-	Nonce        *big.Int     // Meta-Transaction nonce, with encoded space
-	Signature    []byte       // Meta-Transaction signature
-
-	encoded bool
-}
-
-type Transactions []*Transaction
-
-type SignedTransactions struct {
-	ChainID       *big.Int
-	WalletConfig  core.WalletConfig
-	WalletContext sequence.WalletContext
-
-	Transactions Transactions // The meta-transactions
-	Nonce        *big.Int     // Nonce of the transactions
-	Digest       common.Hash  // Digest of the transactions
-	Signature    []byte       // Signature (encoded as bytes from *Signature) of the txn digest
-}
-
-type Relayer interface {
-	// ..
-	GetProvider() *ethrpc.Provider
-
-	// ..
-	EstimateGasLimits(ctx context.Context, walletConfig core.WalletConfig, walletContext sequence.WalletContext, txns Transactions) (Transactions, error)
-
-	// NOTE: nonce space is 160 bits wide
-	GetNonce(ctx context.Context, walletConfig core.WalletConfig, walletContext sequence.WalletContext, space *big.Int, blockNum *big.Int) (*big.Int, error)
-
-	// Relay will submit the Sequence signed meta transaction to the relayer. The method will block until the relayer
-	// responds with the native transaction hash (*types.Transaction), which means the relayer has submitted the transaction
-	// request to the network. Clients can use WaitReceipt to wait until the metaTxnID has been mined.
-	Relay(ctx context.Context, signedTxs *SignedTransactions) (MetaTxnID, *types.Transaction, ethtxn.WaitReceipt, error)
-
-	// ..
-	Wait(ctx context.Context, metaTxnID MetaTxnID, optTimeout ...time.Duration) (MetaTxnStatus, *types.Receipt, error)
-}
-
 func main() {
 
-	// eoa, _ := ethwallet.NewWalletFromRandomEntropy()
-	eoa, _ := ethwallet.NewWalletFromPrivateKey("")
+	// Signer
+	eoa, err := ethwallet.NewWalletFromPrivateKey("private-key-goes-here")
+	if err != nil {
+		log.Fatalf("created wallet failed: %v", err)
+	}
 
 	w, err := sequence.GenericNewWalletSingleOwner[*v2.WalletConfig](eoa, sequence.V2SequenceContext())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// set provider
+	// Setup provider
 	nodeURL := "https://nodes.sequence.app/arbitrum-nova" // Replace with your actual node URL
 	p, err := ethrpc.NewProvider(nodeURL)
 	if err != nil {
@@ -98,26 +44,9 @@ func main() {
 
 	log := logger.NewLogger(logger.LogLevel_DEBUG)
 
-	// Receipts listener
-	monitorOptions := ethmonitor.DefaultOptions
-	monitorOptions.Logger = log
-	monitorOptions.WithLogs = true
-	monitorOptions.BlockRetentionLimit = 400
-	monitorOptions.StartBlockNumber = big.NewInt(-20)
-
-	monitor, err := ethmonitor.NewMonitor(p, monitorOptions)
-	if err != nil {
-		log.Fatalf("monitor create failed: %v", err)
-	}
-
-	receiptsListener, err := ethreceipts.NewReceiptsListener(log, p, monitor)
-	if err != nil {
-		log.Fatalf("receiptlistener create failed: %v", err)
-	}
-
 	// Setup Relayer on the wallet
 	relayerURL := "https://arbitrum-nova-relayer.sequence.app"
-	relayer, err := relayer.NewRpcRelayer(p, receiptsListener, relayerURL, http.DefaultClient)
+	relayer, err := relayer.NewRpcRelayer(p, nil, relayerURL, http.DefaultClient)
 	if err != nil {
 		log.Fatalf("failed to create new rpc relayer client: %v", err)
 	}
@@ -154,7 +83,11 @@ func main() {
 	calldata, _ := parsedAbi.Pack("mint", 100)
 
 	// nonce
-	nonce, _ := w.GetNonce()
+	//
+	// we can either get the next nonce, or generate a random one. By generating
+	// a random one, you're able to send transactions in parallel.
+	// nonce, _ := w.GetNonce()
+	nonce, _ := sequence.GenerateRandomNonce()
 	fmt.Println("nonce:", nonce)
 
 	// txn
